b9f75b1dde43e5977fa60d2536cff327bac91e21
Ivan Perez <ivanpj93@outlook.com>
2023-01-19 19:34:41 -0600

    commit inicial

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+
+        int tabla = 10;
+        for(int i =1; i<=tabla; i++){
+            for(int j= 1; j<=tabla; j++){
+                System.out.println(i +"x" + j + "=" + i*j);
+                if(j==10){
+                    System.out.println("\n");
+                }
+            }
+        }
+
+    }
+}
